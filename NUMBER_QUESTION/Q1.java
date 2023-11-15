69faee55c8f12596b97341f1e40663d8d1eb102e
Ritu Kumari <127641811+ritukumari2004@users.noreply.github.com>
2023-11-15 21:59:09 +0530

    Update Q1.java

--- NUMBER_QUESTION/Q1.java
+++ NUMBER_QUESTION/Q1.java
@@ -1,1 +1,18 @@
+//Program to find GCD or HCF of two numbers
+class Q1
+{
+    public static void main (String[]args)
+    {
+        int num1 = 36, num2 = 60, hcf;
 
+            hcf = getHCF (num1, num2);
+            System.out.println ("The HCF: " + hcf);
+    }
+
+// This method improves complexity of repeated substraction
+// By efficient use of modulo operator in euclidean algorithm
+    static int getHCF (int a, int b)
+    {
+        return b == 0 ? a : getHCF (b, a % b);
+    }
+}
