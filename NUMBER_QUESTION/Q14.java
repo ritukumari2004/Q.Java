0e418037de3dfc0021a99f9fee2848a7be73cfc1
Ritu Kumari <127641811+ritukumari2004@users.noreply.github.com>
2023-11-29 23:12:11 +0530

    Q14.java

---
+++ NUMBER_QUESTION/Q14.java
@@ -0,0 +1,12 @@
+//Maximum number of handshakes
+public class Q14
+{
+	public static void main(String[] args) {
+	     
+    int num = 10;
+
+    int total = num * (num-1) / 2; 
+
+		System.out.println("For "+ num +" people there will be " +total+" handshakes");
+	}
+}
